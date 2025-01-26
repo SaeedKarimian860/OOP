13e2d5b71db42b6d34222d5593ca3a96bd981e5a
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-26 09:35:16 -0500

    Prototypes

---
+++ Prototypes/app.js
@@ -0,0 +1,7 @@
+String.prototype.yell = function () {
+    return `YAY!!!! ${this.toUpperCase()}!!!!!`;
+};
+
+Array.prototype.pop = function () {
+    return "Sorry I want that element, I will never pop it off!";
+} // Creating/manipulating our own version of pop!
